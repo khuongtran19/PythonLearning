7b38d8a61fd34a74ec741cac7d4d6327126e9e8a
khuong <tranvinhkhuong@gmail.com>
2020-05-13 22:25:45 -0700

    new python lesson

--- Python example and practice/__name__ and "__main__"/one.py
+++ Python example and practice/__name__ and "__main__"/one.py
@@ -1,1 +1,15 @@
 # one.py
+
+def func():
+    print('FUNC() in ONE.PY')
+
+print('TOP LEVEL IN ONE.PY')
+
+if __name__ == '__main__':
+    print('ONE.PY is being run directly!')
+else:
+    print('ONE.py has been imported!')
+
+#python one.py
+#TOP LEVEL IN ONE.PY
+#ONE.PY is being run directly!
--- Python example and practice/__name__ and "__main__"/two.py
+++ Python example and practice/__name__ and "__main__"/two.py
@@ -1,1 +1,18 @@
 # two.py
+
+import one
+print('TOP LEVEL IN TWO.PY')
+
+one.func()
+
+if __name__ == '__main__':
+    print('TWO.PY is being run directly!')
+else:
+    print('imported')
+
+#python two.py
+#TOP LEVEL IN ONE.PY
+#ONE.py has been imported!
+#TOP LEVEL IN TWO.PY
+#FUNC() IN ONE.PY
+#TWO.PY is being run directly!
